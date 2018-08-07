cf6728616105ea1849e8a3aea2338f50dab37e66
Marco Palladino <36243627+marcuspalladino@users.noreply.github.com>
2018-08-07 22:56:46 +0200

    Add files via upload

---
+++ src/M28BYJ48.cpp
@@ -0,0 +1,130 @@
+/*---------------------------------------------------------------------------------------*
+ *                                                                                       *
+ *  Library name:   (M)otor 28BYJ48 Vs 1                                                 *
+ *  Author:         Marco Palladino - marck.palladino@gmail.com                          *
+ *  Start date:     04.02.2018 (first beta version)                                      *
+ *  Last Update:    11.03.2018                                                           *
+ *                                                                                       *
+ * This work is licensed under the Creative Commons Attribution-                         *
+ * Attribution-ShareAlike 4.0 International (CC BY-SA 4.0).                              *
+ * To view a copy of this license,                                                       *
+ * visit http://creativecommons.org/licenses/by-sa/4.0/ or send a                        *
+ * letter to Creative Commons, 171 Second Street, Suite 300,                             *
+ * San Francisco, California, 94105, USA.                                                *
+ *                                                                                       *
+ *                                                                                       *
+ * Step Command IN4 IN3 IN2 IN1                                                          *
+ * A  01H   0   0   0   1                                                                *
+ * AB 03H   0   0   1   1                                                                *
+ * B  02H   0   0   1   0                                                                *
+ * BC 06H   0   1   1   0                                                                *
+ * C  04H   0   1   0   0                                                                *
+ * CD 0CH   1   1   0   0                                                                *
+ * D  08H   1   0   0   0                                                                *
+ * DA 09H   1   0   0   1                                                                *
+ *                                                                                       *
+ *                                                                                       *
+ *---------------------------------------------------------------------------------------*/
+ 
+#include <Arduino.h>
+#include "M28BYJ48.h"
+
+// inizialize driver
+M28BYJ48::M28BYJ48(int steps_for_rev, int pin_1, int pin_2,
+                                      int pin_3, int pin_4)
+{
+	//set variables
+	this->steps_for_rev = steps_for_rev;
+	//this->mDirection = 0;
+	this->m_dir = 0;
+	
+	// set pin
+	this->pin_1 = pin_1;
+	this->pin_2 = pin_2;
+	this->pin_3 = pin_3;
+	this->pin_4 = pin_4;
+	
+	// set pin mode
+	pinMode(this->pin_1,OUTPUT);
+	pinMode(this->pin_2,OUTPUT);
+	pinMode(this->pin_3,OUTPUT);
+	pinMode(this->pin_4,OUTPUT);
+}
+// ULN2003 output logic
+void M28BYJ48::logicOut( int IN4,int IN3,int IN2,int IN1)
+{
+  (IN1==1)?digitalWrite(pin_1,1):digitalWrite(pin_1,0);
+  (IN2==1)?digitalWrite(pin_2,1):digitalWrite(pin_2,0);
+  (IN3==1)?digitalWrite(pin_3,1):digitalWrite(pin_3,0);
+  (IN4==1)?digitalWrite(pin_4,1):digitalWrite(pin_4,0);
+}
+
+// 28BYJ48 motor phases
+void M28BYJ48::step(int phase)
+{
+  switch (phase)
+    {
+       case A:  logicOut(0,0,0,1); break;
+       case AB: logicOut(0,0,1,1); break;
+       case B:  logicOut(0,0,1,0); break; 
+       case BC: logicOut(0,1,1,0); break;
+       case C:  logicOut(0,1,0,0); break;
+       case CD: logicOut(1,1,0,0); break;
+       case D:  logicOut(1,0,0,0); break;
+       case DA: logicOut(1,0,0,1); break;
+       case MOTOR_OFF: logicOut(0,0,0,0); break; // OFF motor
+    }
+}
+// 28BYJ48 set rpm
+void M28BYJ48::tuning (unsigned int rpm)
+{
+    // calculate the step pulse
+	  if (rpm > 0) {this->step_pulse = round(60* 1000L * 1000L / this->steps_for_rev / rpm);}
+}
+
+// move motor with steps
+void M28BYJ48::moveSteps (unsigned long steps, unsigned int m_dir)
+{
+   // set counter for steps left
+   int steps_left = abs(steps);
+   
+	 // determin direction
+   this->m_dir = m_dir;
+	 
+	 // loop for call phases case
+	 while (steps_left > 0)
+	 {
+   // set timer
+	 unsigned long timer = micros();
+   
+   // move with the pulse timer
+   if (timer - this->last_time >= this->step_pulse)
+   {
+    
+   // reset timer
+   this->last_time = timer;
+	 if (this->m_dir==1) {this->phase+=1; if (this->phase == this->steps_for_rev){this->phase = 0;}}
+   else {if (this->phase == 0) {this->phase = this->steps_for_rev;} this->phase-=1;
+   }
+   steps_left--; // decrement step left counter
+   
+   // change phase
+	 step(this->phase % 8);
+   }
+  }
+}
+// move motor with degree
+void M28BYJ48::moveDeg (float deg, unsigned int m_dir)
+{
+  (deg == 'MOTOR_OFF')?step(MOTOR_OFF):moveSteps(round(deg * this->steps_for_rev / 360), m_dir);
+}
+
+void M28BYJ48::motorStop(void) {
+  step(MOTOR_OFF);
+}
+
+// version() returns the version of the library
+int M28BYJ48::version(void)
+{
+  return 2;
+}
---
+++ src/M28BYJ48.h
@@ -0,0 +1,75 @@
+/*---------------------------------------------------------------------------------------*
+ *                                                                                       *
+ *  Library name:   (M)otor 28BYJ48 Vs 1                                                 *
+ *  Author:         Marco Palladino - marck.palladino@gmail.com                          *
+ *  Start date:     04.02.2018 (first beta version)                                      *
+ *  Last Update:    11.03.2018                                                           *
+ *                                                                                       *
+ * This work is licensed under the Creative Commons Attribution-                         *
+ * Attribution-ShareAlike 4.0 International (CC BY-SA 4.0).                              *
+ * To view a copy of this license,                                                       *
+ * visit http://creativecommons.org/licenses/by-sa/4.0/ or send a                        *
+ * letter to Creative Commons, 171 Second Street, Suite 300,                             *
+ * San Francisco, California, 94105, USA.                                                *
+ *                                                                                       *
+ *                                                                                       *
+ * Step Command IN4 IN3 IN2 IN1                                                          *
+ * A  01H   0   0   0   1                                                                *
+ * AB 03H   0   0   1   1                                                                *
+ * B  02H   0   0   1   0                                                                *
+ * BC 06H   0   1   1   0                                                                *
+ * C  04H   0   1   0   0                                                                *
+ * CD 0CH   1   1   0   0                                                                *
+ * D  08H   1   0   0   0                                                                *
+ * DA 09H   1   0   0   1                                                                *
+ *                                                                                       *
+ *                                                                                       *
+ *---------------------------------------------------------------------------------------*/
+ 
+#ifndef M28BYJ48_h
+#define M28BYJ48_h
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+// library interface description
+class M28BYJ48 {
+  public:
+    // constructor:
+    M28BYJ48(int steps_for_rev, int pin_1, int pin_2,
+                                int pin_3, int pin_4);
+    
+    // mover method:
+    void tuning (unsigned int rpm);                                // set rpm
+                                                                   // don't fix rpm for external command application (ex grbl)
+    void moveSteps (unsigned long steps, unsigned int m_dir);      // move in steps
+    void moveDeg (float deg, unsigned int m_dir);                  // move in degree
+    void moveMm (float mm, unsigned int m_dir);                    // next upgrade move in mm
+    void motorStop (void);                                         // stop motor
+    
+    int version(void);
+    enum Motor {A, AB, B, BC, C, CD, D, DA, MOTOR_OFF};
+    enum m_dir {CW, CCW}; // <------------------------NEXT UPDATE
+    
+  private:
+    void logicOut( int pin_4,int pin_3,int pin_2,int pin_1);
+    void step(int phase);
+    
+    int steps_for_rev;
+    unsigned int m_dir;
+    unsigned long step_pulse;
+    unsigned long last_time;
+    int phase;
+    
+    // motor pin numbers:
+    int pin_1;
+    int pin_2;
+    int pin_3;
+    int pin_4;
+    
+};
+
+#endif // M28BYJ48_h
